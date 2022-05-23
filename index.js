7ac4e4b9bbef4969e83aa8549233590222e30b84
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-23 14:06:25 +0600

    implemented user route and jwt

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
+
+  if (!authHeader) {
+    return res.status(401).send({
+      message: "unauthorized access",
+    });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    } else {
+      console.log("decoded", decoded);
+      req.decoded = decoded;
+      next();
+    }
+  });
+};
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dru1h.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -17,19 +38,43 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   client.close();
-// });
-
 const run = async () => {
   try {
     await client.connect();
     console.log("strap server is running");
     const productsCollection = client.db("straptools").collection("products");
+    const userCollection = client.db("straptools").collection("users");
 
     app.get("/", async (req, res) => {
       res.send("server is running");
+    });
+
+    // Users
+    //post - will use when login and signup
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+
+      const filter = { email };
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: user,
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send({ result, token });
+    });
+
+    // to get all the user
+    app.get("/users", verifyJwt, async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
     });
   } finally {
   }
